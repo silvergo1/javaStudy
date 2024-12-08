797aa38b4b32a77eb8d972c8a35d65a1bd6549ab
goeun <ohss0613@naver.com>
2024-12-09 08:24:41 +0900

    실전 자바 - 기본편 : 클래스와 데이터 문제와 풀이 (인프런)

---
+++ java-basic/src/class1/ex/MovieReviewMain1.java
@@ -0,0 +1,18 @@
+package class1.ex;
+
+public class MovieReviewMain1 {
+
+    public static void main(String[] args) {
+        MovieReview inception = new MovieReview();
+        inception.title = "인샙션";
+        inception.review = "인생은 무한루프";
+
+        MovieReview aboutTime = new MovieReview();
+        aboutTime.title = "어바웃 타임";
+        aboutTime.review = "인생은 시간 영화!";
+
+        System.out.println("영화 제목 : " + inception.title + "리뷰 : " + inception.review);
+        System.out.println("영화 제목 : " + aboutTime.title + "리뷰 : " + aboutTime.review);
+    }
+
+}
