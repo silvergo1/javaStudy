2a412f5cf688e577299e30f6bc76198b4c27174d
goeun <ohss0613@naver.com>
2025-01-01 19:57:06 +0900

    실전 자바 - 기본편 : 변수와 초기화, null, NullPointerException, 문제와 풀이, 정리 (인프런)

---
+++ java-basic/src/ref/BigData.java
@@ -0,0 +1,6 @@
+package ref;
+
+public class BigData {
+    Data data; // null (객체라서 기본값 null)
+    int count; // 0
+}
---
+++ java-basic/src/ref/InitMain.java
@@ -0,0 +1,13 @@
+package ref;
+
+public class InitMain {
+
+    public static void main(String[] args) {
+
+        Initdata data = new Initdata();
+        System.out.println("value1 = " + data.value1);
+        System.out.println("value2 = " + data.value2);
+
+    }
+
+}
