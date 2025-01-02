5752b9098cec45eabfd4e04a466478463820fd1f
goeun <ohss0613@naver.com>
2025-01-02 22:19:41 +0900

    실전 자바 - 기본편 : 생성자 (인프런)

---
+++ java-basic/src/construct/MemberThis.java
@@ -0,0 +1,11 @@
+package construct;
+
+public class MemberThis {
+
+    String nameField;
+
+    void initMember(String nameParameter) {
+        nameField = nameParameter; // nameField this 생략 가능
+    }
+
+}
